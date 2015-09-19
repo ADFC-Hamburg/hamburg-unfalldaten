9c2d314365b93e6034d2b3e80bb8121b2300178b
Sven Anders <sven@anders.hamburg>
2015-09-19 10:19:18 +0200

    jscs fix

--- js/view/comment.js
+++ js/view/comment.js
@@ -22,12 +22,12 @@ function clearErr() {
 
 function addComment() {
     var data= {
-	id:$("#comment-id").val(),
-	usr:$("#comment-usr").val(),
-	email:$("#comment-email").val(),
-	save:$("#comment-save").is(":checked"),
-	subject:$("#comment-subject").val(),
-	comment:$("#comment-comment").val(),
+	id:$('#comment-id').val(),
+	usr:$('#comment-usr').val(),
+	email:$('#comment-email').val(),
+	save:$('#comment-save').is(':checked'),
+	subject:$('#comment-subject').val(),
+	comment:$('#comment-comment').val(),
     };
 
     if (data.save) {
@@ -38,24 +38,31 @@ function addComment() {
 	Cookies.remove('adfc_useremail');
     }
 
-    $('#comment-send-btn').button("loading");
+    $('#comment-send-btn').button('loading');
     clearErr();
     $('#newcomment').hide();
-    $.post( "api/comment.php/new", JSON.stringify(data) ).done( function (d) {
+    $.post( 'api/comment.php/new', JSON.stringify(data) ).done( function (d) {
 	console.log('done',d);
 	d=JSON.parse(d);
-        $('#comment-send-btn').button("reset");
+        $('#comment-send-btn').button('reset');
 	if (d.status==1) {
 	    $('#comment-send-btn').hide();
-	    $('#newcommentmsg').removeClass().addClass('alert alert-success').text("Vielen Dank, der Kommentar wurde gespeichert. Wir melden uns bei Ihnen per E-Mail sobald dieser angezeigt wird.").show();
+	    $('#newcommentmsg').removeClass().
+		addClass('alert alert-success')
+		.text('Vielen Dank, der Kommentar wurde gespeichert. Wir melden'+
+		      ' uns bei Ihnen per E-Mail sobald dieser angezeigt wird.')
+		.show();
 	} else if (d.status==2) {
-	    $('#comment-send-btn').button("reset");
+	    $('#comment-send-btn').button('reset');
 	    $('#newcomment').show();
 	    $.map(d.val, function(value, index) {
 		var obj=$('#fg-'+index);
-		obj.addClass("has-error").addClass("has-feedback").append($('<span>').addClass('glyphicon glyphicon-remove form-control-feedback'));
+		obj.addClass('has-error')
+		    .addClass('has-feedback')
+		    .append($('<span>')
+			    .addClass('glyphicon glyphicon-remove form-control-feedback'));
 		for (var i = 0; i < value.length; i++) {
-		    obj.append($('<span>').addClass("help-block").text(value[i]));
+		    obj.append($('<span>').addClass('help-block').text(value[i]));
 		}
 //		console.log(value,index);
 	    });
@@ -66,9 +73,11 @@ function addComment() {
 	}
     }).fail(function (jqXHR, textStatus, errorThrown ) {
 	$('#newcomment').show();
-	$('#comment-send-btn').button("reset");
+	$('#comment-send-btn').button('reset');
 	console.log('fail', jqXHR, textStatus, errorThrown);
-	$('#newcommentmsg').removeClass().addClass('alert alert-danger').text("Leider gab es beim Speichern einen unvorhergesehen Fehler. Bitte informieren Sie den Admin unter: adfc2015@sven.anders.hamburg").show();
+	$('#newcommentmsg').removeClass().addClass('alert alert-danger')
+	    .text('Leider gab es beim Speichern einen unvorhergesehen Fehler.'+
+		  ' Bitte informieren Sie den Admin unter: adfc2015@sven.anders.hamburg').show();
     });
 }
 		      $('#comment-send-btn').on('click', addComment);
@@ -76,12 +85,12 @@ function convertDate(date) {
     var pattern = /^(\d\d\d\d)-(\d\d)-(\d\d) .*$/;
     var matches = pattern.exec(date);
     if (!matches) {
-        throw new Error("Invalid string: " + date);
+        throw new Error('Invalid string: ' + date);
     }
     var year = matches[1];
     var month = matches[2];  
     var day = matches[3];
-    return day+"."+month+"."+year;
+    return day+'.'+month+'.'+year;
 }
 function loadComments( lfnr ) {
     $('#oldcomments').text('Lade Kommentare ...');
@@ -98,13 +107,16 @@ function loadComments( lfnr ) {
 	    var container= $('#oldcomments');
 	    container.html('');
 	    if (comments.length===0) {
-		container.append($('<div>').addClass('alert alert-success').text('Wir haben leider noch keine Kommentare zur Unfallstelle erhalten. Schreibe den ersten Kommentar!'));
+		container.append($('<div>').addClass('alert alert-success')
+				 .text('Wir haben leider noch keine Kommentare zur Unfallstelle erhalten.'+
+				       ' Schreibe den ersten Kommentar!'));
 	    } else {
 		for (var i = 0; i < comments.length; i++) {
 		    var comment= comments[i];
 		    container.append($('<h4>').text(comment.subject));
 		    
-		    container.append($('<div id="comment-from">').text('Von '+comment.creator+' am '+convertDate(comment.created.date)+'.'));
+		    container.append($('<div id="comment-from">')
+				     .text('Von '+comment.creator+' am '+convertDate(comment.created.date)+'.'));
 		    container.append($('<div id="comment-msg">').text(comment.description));
 		    container.append($('<hr>'));
 		}
@@ -118,7 +130,7 @@ function openComment( id ) {
     loadComments(id);
     $('#comment-send-btn').show();
     $('#newcomment').show();
-    $('#newcommentmsg').text("");
+    $('#newcommentmsg').text('');
     $('#comment-subject').val('');
     $('#comment-usr').val('');
     $('#comment-email').val('');
@@ -126,11 +138,11 @@ function openComment( id ) {
     if (Cookies.get('adfc_username')) {
 	$('#comment-usr').val(Cookies.get('adfc_username'));
 	$('#comment-email').val(Cookies.get('adfc_useremail'));
-	$("#comment-save").prop( "checked",true);
+	$('#comment-save').prop( 'checked',true);
     }
-    $("#comment-title").text("Kommentare zu Fahrradunfall Nr. "+id+" in 2014");
-    $("#comment-id").val(id);
-    $("#comments").modal();
+    $('#comment-title').text('Kommentare zu Fahrradunfall Nr. '+id+' in 2014');
+    $('#comment-id').val(id);
+    $('#comments').modal();
 }
 
 return {
