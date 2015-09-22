7f151ca9293d0503135472eaed3981e015688cc8
Sven Anders <sven@anders.hamburg>
2015-09-22 09:00:32 +0200

    extended update skript

--- update.sh
+++ update.sh
@@ -3,4 +3,10 @@ git pull
 grunt
 cd api
 /home/sven/composer.phar install
-vendor/bin/doctrine orm:schema-tool:update
+./vendor/bin/doctrine orm:schema-tool:update
+cd ..
+grunt clean
+grunt
+rm -rf /var/www/anders.hamburg/adfc/test.old
+mv /var/www/anders.hamburg/adfc/test /var/www/anders.hamburg/adfc/test.old
+cp -r dist /var/www/anders.hamburg/adfc/test
